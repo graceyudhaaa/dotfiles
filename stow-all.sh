53ce87b2574e44c6dda320ba9badb06c8deaf1ae
graceyudhaaa <emmanuelgrace005@gmail.com>
2025-06-18 00:31:55 +0700

    Reorder directories in stow-all.sh for better organization

--- stow-all.sh
+++ stow-all.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 dirs=(
-  fish
-  sxhkd
-  rofi
-  polybar
-  mpv
-  starship
   autostart
-  picom
+  bash
+  fish
   kitty
+  mpv
+  picom
+  polybar
+  rofi
+  starship
+  sxhkd
 )
 for dir in "${dirs[@]}"; do
   stow --target="$HOME" "$dir"
